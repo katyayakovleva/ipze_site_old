04461f7868ad64cc1adcabc7724cc9c93665598b
sobolv_a <lera.sobol.26@gmail.com>
2022-03-31 01:59:39 +0300

    Доналаштування меню

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,37 @@
+
 $(document).ready(function (){
     $('.head_menu ul ul, .head_menu ul ul ul').addClass('submenu');
     $('.head_menu ul').addClass('topmenu');
+    var windowWidth = $('body').innerWidth();
+    if(windowWidth < 1202){
+        $('.head_menu').addClass('head_burger_menu').css("display", "none");
+    }
+    else{
+        $('.head_menu').removeClass('head_burger_menu').css("display", "block");
+    }
+    $(window).resize(function (){
+        windowWidth = $('body').innerWidth();
+        if(windowWidth < 1202){
+            $('.head_menu').addClass('head_burger_menu').css("display", "none");
+        }
+        else{
+            $('.head_menu').removeClass('head_burger_menu').css("display", "block");
+            $('.head_menu li button').remove();
+        }
+    });
     $('.header_burger').click(function (event){
        $('.header_burger, .menu_back').toggleClass('active');
-       // $('.logo_img').toggleClass('none');
-       $('.head_menu').slideToggle();
+       $('.head_burger_menu').slideToggle();
+       $(".head_burger_menu ul li").each(function() {
+           this_li = $(this);
+           if ($(this_li).find("ul").length > 0) {
+               $(this_li).append('<button>v</button>');
+           }
+       });
+       $('.head_burger_menu li button').click(function() {
+           $(this).closest('li').find('ul').eq(0).slideToggle();
+       });
     });
  });
- 
+
  
