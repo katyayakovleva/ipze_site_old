0bd0917fa600b68207538ccc5d37ba44a62c3ee6
katya <51397341+katyayakovleva@users.noreply.github.com>
2022-03-31 15:58:26 +0300

    Додання функціоналу у форму пошуку

--- searchform.php
+++ searchform.php
@@ -9,8 +9,8 @@
  */
 
 ?>
-<form>
-  <input type="text" placeholder="Пошук...">
+<form role="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  <input type="text" placeholder="Пошук..." id="search-input" name="s" value="<?php echo esc_attr( get_search_query() ); ?>">
   <button type="submit"></button>
 </form>
  
